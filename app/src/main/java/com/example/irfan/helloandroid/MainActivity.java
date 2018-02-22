8d6019383d45b1b180ba14670f05c55f6378281c
Irfan Hamid <irfnhm@gmail.com>
2018-02-22 18:49:59 +0530

    Added project files

---
+++ app/src/main/java/com/example/irfan/helloandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.irfan.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
